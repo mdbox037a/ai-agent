41e0f760a492a7fc387711b68303a95304496a55
matt box <mdbox037a@protonmail.com>
2025-10-27 21:00:15 -0400

    lay groundwork for remembering past prompts as part of continuous conversation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,8 +12,12 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=sys.argv[1]
+        model="gemini-2.0-flash-001",
+        contents=messages,
     )
     print(response.text)
 
