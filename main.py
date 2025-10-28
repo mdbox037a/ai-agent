c163cf3b4afafa53be7eeff5063bdb3d3f533cab
matt box <mdbox037a@protonmail.com>
2025-10-27 21:21:04 -0400

    implement --verbose flag and add logic to distinguish between optional arguments and user prompt

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    verbose = False
+    if "--verbose" in sys.argv:
+        verbose = True
+    prompt_index = 1
+    for i in range(len(sys.argv)):
+        if "--" not in sys.argv[i]:
+            prompt_index = i
+    user_prompt = sys.argv[prompt_index]
+
     messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -21,9 +31,11 @@ def main():
     )
     print(response.text)
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+    if verbose is True:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
