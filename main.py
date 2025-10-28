5b5efefc44b130a27dfa18b57ea0f5f543604dec
matt box <mdbox037a@protonmail.com>
2025-10-27 20:50:08 -0400

    alter code to accept prompt from command line and exit 1 if no prompt given

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    if sys.argv[1] is None:
+        print("error: no prompt supplied")
+        sys.exit(1)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model="gemini-2.0-flash-001", contents=sys.argv[1]
     )
     print(response.text)
 
