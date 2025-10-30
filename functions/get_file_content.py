7dee9a11eb19ed58238bc1b1cec0bd136807bcca
matt box <mdbox037a@protonmail.com>
2025-10-29 22:25:49 -0400

    add config.py to store configs and make small edit to get_file_content to use that file for MAX_CHARS

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
@@ -9,7 +10,6 @@ def get_file_content(working_directory, file_path):
         if not os.path.isfile(full_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
-        MAX_CHARS = 10000
         with open(full_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
         if len(file_content_string) == MAX_CHARS:
