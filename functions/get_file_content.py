308a96fb57fe7d31f853d05d879a0149732d6938
matt box <mdbox037a@protonmail.com>
2025-10-29 21:53:06 -0400

    complete first draft of file content reader function

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not full_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(full_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        MAX_CHARS = 10000
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) == MAX_CHARS:
+            file_content_string = (
+                file_content_string
+                + f'\n[...File "{file_path}" truncated at 10000 characters]'
+            )
+    except Exception as e:
+        print(f"Error: {e}")
