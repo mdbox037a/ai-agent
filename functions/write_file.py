bcac05e49a43df2cd90e7ad9a6aba6b5a0e8ca07
matt box <mdbox037a@protonmail.com>
2025-10-30 19:31:57 -0400

    implement fix for creating non-extant directories and modify test script

--- functions/write_file.py
+++ functions/write_file.py
@@ -6,8 +6,8 @@ def write_file(working_directory, file_path, content):
         full_path = os.path.abspath(os.path.join(working_directory, file_path))
         if not full_path.startswith(os.path.abspath(working_directory)):
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.exists(full_path):
-            os.makedirs(full_path)
+        if not os.path.exists(os.path.dirname(full_path)):
+            os.makedirs(os.path.dirname(full_path))
 
         with open(full_path, "w") as f:
             f.write(content)
