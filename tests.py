d19377905110fa9c6f158c82c73b22660fdf2c8f
matt box <mdbox037a@protonmail.com>
2025-10-29 21:57:39 -0400

    rework test script to test new get_file_content function

--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Result for current directory:")
-print(f"{get_files_info('calculator', '.')}")
+# print("Result for current directory:")
+# print(f"{get_files_info('calculator', '.')}")
+#
+# print("Result for 'pkg' directory:")
+# print(f"{get_files_info('calculator', 'pkg')}")
+#
+# print("Result for '/bin' directory:")
+# print(f"{get_files_info('calculator', '/bin')}")
+#
+# print("Result for '../' directory:")
+# print(f"{get_files_info('calculator', '../')}")
 
-print("Result for 'pkg' directory:")
-print(f"{get_files_info('calculator', 'pkg')}")
-
-print("Result for '/bin' directory:")
-print(f"{get_files_info('calculator', '/bin')}")
-
-print("Result for '../' directory:")
-print(f"{get_files_info('calculator', '../')}")
+print(get_file_content("calculator", "lorem.txt"))
