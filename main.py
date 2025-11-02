979b170fd684dee770fa1df5c4e2640b19b3280a
matt box <mdbox037a@protonmail.com>
2025-11-02 15:07:11 -0500

    allow LLM to use functions - starting with get_files_info; modify system prompt to match in config.py and add directives to reach function in main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import system_prompt
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -22,15 +23,26 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
-    print(response.text)
+    if response.function_calls is not None:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
 
     if verbose is True:
         prompt_tokens = response.usage_metadata.prompt_token_count
