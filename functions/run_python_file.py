3a81c61e8ad458dc941f31c76db0d98ce6c937bc
matt box <mdbox037a@protonmail.com>
2025-11-03 17:52:59 -0500

    add schema to allow LLM to run python files and adapt main to fit

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 
 def run_python_file(working_directory, file_path, args=[]):
@@ -34,3 +35,24 @@ def run_python_file(working_directory, file_path, args=[]):
         return output
     except Exception as e:
         print(f"Error: executing Python file: {e}")
+
+
+# ai agent schema information
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run a python program within the file at file_path, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the python program file, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.STRING,
+                description="Any arguments to be supplied as arguments for the python program to be run, if any.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from config import system_prompt
 from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.write_file import schema_write_file
+from functions.run_python_file import schema_run_python_file
 
 
 def main():
@@ -29,6 +30,7 @@ def main():
         function_declarations=[
             schema_get_files_info,
             schema_get_file_content,
+            schema_run_python_file,
             schema_write_file,
         ]
     )
